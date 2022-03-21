12a24ebead342bf36d410027a8b4eb3a4ab4482d
jerryc214 <jerrychen214@gmail.com>
2022-03-20 21:30:36 -0700

    trying to pull chicken

---
+++ DystopianAdventure/Assets/Scripts/Select.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour
+{
+    private Renderer selectRender;
+    private bool rayHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rayHit = false;
+      selectRender = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+      if(rayHit)
+      {
+        selectRender.material.color = Color.blue;
+        rayHit = false;
+      }
+      else
+      {
+        selectRender.material.color = Color.white;
+      }
+    }
+
+    void HitByRay()
+    {
+      Debug.Log("Hit");
+      rayHit = true;
+    }
+}
--- DystopianAdventure/Assets/Scripts/SelectionManager.cs
+++ DystopianAdventure/Assets/Scripts/SelectionManager.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
-    [SerializeField] private string selectableTag = "Selectable";
-    [SerializeField] private Material highlightMaterial;
-    [SerializeField] private Material defaultMaterial;
-
-    private Transform _selection;
     public Transform playerAxis;
 
     // Update is called once per frame
-    //Code from https://www.youtube.com/watch?v=_yf5vzZ2sYE
     void Update()
     {
-      if(_selection != null)
-      {
-        var selectionRenderer  = _selection.GetComponent<Renderer>();
-        selectionRenderer.material = defaultMaterial;
-        _selection = null;
-      }
       RaycastHit hit;
       if(Physics.Raycast(playerAxis.transform.position, playerAxis.transform.forward, out hit))
       {
-        var selection = hit.transform;
-        if(selection.CompareTag(selectableTag))
-        {
-          var selectionRenderer = selection.GetComponent<Renderer>();
-          if(selectionRenderer != null)
-          {
-            selectionRenderer.material = highlightMaterial;
-          }
-          _selection = selection;
-        }
+        hit.transform.SendMessage ("HitByRay");
       }
     }
 }
